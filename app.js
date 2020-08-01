b48d2e1c290cd8394b4abe2f532c5223fc460b5a
PrashantRayamajhi <prashanrayamajhi@gmail.com>
2020-08-01 22:19:45 +0545

    managed route path

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 
 // -------------------------------- Home Route ------------------------------ //
-app.use("/", require("./routes/home"));
+app.use("/", require(__dirname + "/routes/home"));
 
 app.listen(process.env.PORT || 3000, console.log("Listening on port 3000"));
