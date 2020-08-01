3110c643c09d69828c5a544b446c0f5696a086ee
PrashantRayamajhi <prashanrayamajhi@gmail.com>
2020-08-01 21:27:46 +0545

    modified port

--- app.js
+++ app.js
@@ -39,4 +39,4 @@ app.use(expressLayouts);
 // -------------------------------- Home Route ------------------------------ //
 app.use("/", require("./routes/home"));
 
-app.listen(3000 || process.env.PORT, console.log("Listening on port 3000"));
+app.listen(process.env.PORT || 3000, console.log("Listening on port 3000"));
