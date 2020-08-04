0375b34a6ea69de7c4b0da611e41b49231bf6926
PrashantRayamajhi <prashanrayamajhi@gmail.com>
2020-08-04 11:13:44 +0545

    configured confession routes and models

---
+++ models/Confessions.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const ConfessionSchema = new mongoose.Schema({
+  //   username: {
+  //     type: String,
+  //     required: true,
+  //   },
+  title: {
+    type: String,
+    required: true,
+  },
+  confession: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Confession", ConfessionSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Confession = require("./../models/Confessions");
 
 router.get("/", checkAuthenticated, (req, res) => {
-  res.render("user/home.ejs");
+  Confession.find((err, confessions) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("user/home.ejs", { confessions: confessions });
+    }
+  });
 });
 
 router.get("/submit", (req, res) => {
@@ -36,20 +43,35 @@ router.post("/submit", (req, res) => {
   const errors = [];
   let title = req.body.title;
   let confession = req.body.confession;
-
   title = title.trim();
-  confession = title.trim();
+  confession = confession.trim();
 
-  if (title === "") {
+  if (title === "" || title === undefined) {
     errors.push({ msg: "Title cannot be empty" });
   }
 
-  if (confession === "") {
+  if (title.length >= 55) {
+    errors.push({ msg: "Title too long!" });
+  }
+
+  if (confession === "" || confession === undefined) {
     errors.push({ msg: "Confession cannot be empty" });
   }
 
   if (errors.length > 0) {
     res.render("user/submit", { errors, title, confession });
+  } else {
+    const c = new Confession({
+      title: title,
+      confession: confession,
+    });
+    c.save()
+      .then((text) => {
+        res.redirect("/login");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   }
 });
 
